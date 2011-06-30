8dbbfdeb752d59498fec8ba728ca4e53e7e0fca7
Scott McLeod <smcleod@zeddworks.com>
2011-06-30 16:13:46 -0400

    Remove required from resources

--- resources/db.rb
+++ resources/db.rb
@@ -6,5 +6,5 @@ end
 actions :create
 actions :drop
 
-attribute :database, :kind_of => String, :required => true, :name_attribute => true
+attribute :database, :kind_of => String, :name_attribute => true
 attribute :owner, :kind_of => String
--- resources/user.rb
+++ resources/user.rb
@@ -6,5 +6,5 @@ end
 actions :create
 actions :drop
 
-attribute :user, :kind_of => String, :required => true, :name_attribute => true
+attribute :user, :kind_of => String, :name_attribute => true
 attribute :password, :kind_of => String
